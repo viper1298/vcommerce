c8a67d76be889fc7c5b0f395de4870aab32d3811
viper1298 <techguy8527@gmail.com>
2017-05-20 16:10:49 +0530

    first

---
+++ admin_area/delete_pro.php
@@ -0,0 +1,30 @@
+<?php
+
+	include("includes/db.php");
+	if(!isset($_SESSION['user_email']))
+	{
+		echo "<script>window.open('login.php?not_admin=You are not an admin!','_self')</script>";
+	}
+	else {
+
+	if(isset($_GET['delete_pro']))
+		{
+			$delete_id = $_GET['delete_pro'];
+
+			$delete_pro = "delete  from products where product_id = '$delete_id'";
+
+			$run_delete = mysqli_query($con,$delete_pro);
+
+			if($run_delete)
+			{
+				echo "<script>alert('A product has been deleted!')</script>";
+				echo "<script>window.open('index.php?view_products','_self')</script>";
+			}
+		}
+
+
+
+
+
+?>
+<?php }?>
