2264db198290af6f6dfa95d69a1ebc3ce9c52e15
viper1298 <techguy8527@gmail.com>
2017-05-20 16:04:33 +0530

    delete customer

---
+++ admin_area/delete_c.php
@@ -0,0 +1,30 @@
+<?php
+
+	include("includes/db.php");
+	if(!isset($_SESSION['user_email']))
+	{
+		echo "<script>window.open('login.php?not_admin=You are not an admin!','_self')</script>";
+	}
+	else {
+
+	if(isset($_GET['delete_c']))
+		{
+			$delete_id = $_GET['delete_c'];
+
+			$delete_c = "delete  from customers where customer_id = '$delete_id'";
+
+			$run_delete = mysqli_query($con,$delete_c);
+
+			if($run_delete)
+			{
+				echo "<script>alert('A Customer has been deleted!')</script>";
+				echo "<script>window.open('index.php?view_customers','_self')</script>";
+			}
+		}
+
+
+
+
+
+?>
+<?php }?>
